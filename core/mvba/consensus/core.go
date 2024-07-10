ea4a37d3f1855896b1364a367a26d1933cbad6b7
dczZ <dcz1192446141@163.com>
2024-07-10 15:58:57 +0800

    mvba core

--- core/mvba/consensus/core.go
+++ core/mvba/consensus/core.go
@@ -1,1 +1,41 @@
 package consensus
+
+import (
+	"bft/mvba/core"
+	"bft/mvba/crypto"
+	"bft/mvba/pool"
+	"bft/mvba/store"
+)
+
+type Core struct {
+	Name       core.NodeID
+	Committee  core.Committee
+	Parameters core.Parameters
+	SigService *crypto.SigService
+	Store      *store.Store
+	TxPool     *pool.Pool
+	Transimtor *core.Transmitor
+	CallBack   chan struct{}
+}
+
+func NewCore(
+	name core.NodeID,
+	committee core.Committee,
+	parameters core.Parameters,
+	SigService *crypto.SigService,
+	Store *store.Store,
+	TxPool *pool.Pool,
+	Transimtor *core.Transmitor,
+) *Core {
+	core := &Core{
+		Name:       name,
+		Committee:  committee,
+		Parameters: parameters,
+		SigService: SigService,
+		Store:      Store,
+		TxPool:     TxPool,
+		Transimtor: Transimtor,
+	}
+
+	return core
+}
